11c4bed64c2a41a6a3580859d6a4148580983760
Darsh Gandhi <darshg0219@gmail.com>
2025-02-01 23:40:10 -0500

    Made get prime algorithm more efficient

--- assignment_01.py
+++ assignment_01.py
@@ -6,14 +6,14 @@ def get_all_primes(limit):
     prime_list.append(2)
     for i in range(3, limit+1, 2):
         is_prime = True
-        for j in range(3, i+1, 2):
+        for j in range(3, int(math.sqrt(i))+1, 2):
             if i % j == 0 and i != j:
                 is_prime = False
         if is_prime:
             prime_list.append(i)
     return prime_list
 
-print(get_all_primes(200))
+print(get_all_primes(1000000))
 
 def find_largest_gap(limit) :
     return 0
